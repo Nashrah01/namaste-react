5ad0bd70837fb888fc60b3b2ac280aab8f44dfce
Nashrah Sabi <nashrahsabi@Nashrahs-MacBook-Pro.local>
2023-04-18 12:20:37 +0400

    prod build using parcel

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const heading = React.createElement("h1",
 { id: "heading" },
  "Hello World from React!");
@@ -6,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const nestedHead = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"}, 
-[React.createElement("h1", {}, "I'm a H1 tag!"),
+[React.createElement("h1", {}, "This is Namaste React🚀"),
 React.createElement("h2", {}, "I'm a H2 tag!")]),
 React.createElement("div", {id: "child2"}, 
 [React.createElement("h1", {}, "I'm a H1 tag!"), React.createElement("h2", {}, "I'm a H2 tag!")]
