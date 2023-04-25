ad9c27f446bcd7774831b494e2f457910d7915e7
Nashrah Sabi <nashrahsabi@Nashrahs-MacBook-Pro.local>
2023-04-25 16:54:52 +0400

    script changes

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ import ReactDOM  from "react-dom/client";
 
 const heading = React.createElement("h1",
 { id: "heading" },
- "Hello World from React!");
+ "Namaste React🚀");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const nestedHead = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {}, "I'm a H1 tag!"), React.createElement("h2", {}, "I'm a H2 tag!")]));
-
-const nestedHead = React.createElement("div", {id: "parent"}, 
-[React.createElement("div", {id: "child1"}, 
-[React.createElement("h1", {}, "This is Namaste React🚀"),
-React.createElement("h2", {}, "I'm a H2 tag!")]),
-React.createElement("div", {id: "child2"}, 
-[React.createElement("h1", {}, "I'm a H1 tag!"), React.createElement("h2", {}, "I'm a H2 tag!")]
-)]);
-
-root.render(nestedHead);
+root.render(heading);
