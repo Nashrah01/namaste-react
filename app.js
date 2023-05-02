d154a5fbabeb9aebf8274ae0825116869ade2e40
Nashrah <sabi.nashrah@gmail.com>
2023-05-02 18:57:18 +0400

    episode 3 functional component episode 3 functional component

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ import ReactDOM  from "react-dom/client";
 const heading = React.createElement("h1",
 { id: "heading" },
  "Namaste React🚀");
+console.log(heading);
+//JSX
+
+const jsxHeading = <h1 className="head" tabIndex="1" >Namaste React from JSX🚀</h1>;
+console.log(jsxHeading);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
+
+
+
+
