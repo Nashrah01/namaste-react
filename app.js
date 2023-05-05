1dfcf2db69c2972c1b113761934f5a0e33ef81ef
Nashrah <sabi.nashrah@gmail.com>
2023-05-05 15:58:35 +0400

    Episode 3

--- app.js
+++ app.js
@@ -7,13 +7,37 @@ const heading = React.createElement("h1",
 console.log(heading);
 //JSX
 
-const jsxHeading = <h1 className="head" tabIndex="1" >Namaste React from JSX🚀</h1>;
-console.log(jsxHeading);
+const Title = function(){
+    return (
+         <h1 className="head" tabIndex="1" >Namaste React from JSX🚀</h1>);
+        };
 
+
+//React Functional Component
+const num = 200;
+const HeadingComponent = () => {
+    return (
+    <React.Fragment>
+        <div className="container">
+            <h1>{heading}</h1>
+            {Title()}
+            <Title/>
+            <h1>Namaste React Functional Component🚀</h1>
+        </div>
+        <div className="container2">
+            <h1>Hey, I am container 2!</h1>
+        </div>
+    </React.Fragment>
+    )
+};
+
+const HeadingComponent2 = () => (
+    <h1>Namaste React Functional Component🚀</h1>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
 
+//Coding Assignment Episode 3
 
-
-
+const head = React.createElement("div", {classNmae:"title"}, [React.createElement("h1", {}, "H1 Tag!"), React.createElement("h2", {}, "H2 Tag!"), React.createElement("h3", {}, "H3 Tag!") ])
